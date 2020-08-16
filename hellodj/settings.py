b9e0f01b2abbfcad8f723fda734a22e980cc68ca
MD Mahabub Alam <mahabub723405@gmail.com>
2020-08-17 00:13:51 +0600

    re-tried 222

--- hellodj/settings.py
+++ hellodj/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
     '/var/www/static/',
 ]
 
-# Heroku support
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
